f798dfccd53e33e01ac845fcae080bd775a29d7b
sy2729 <sy2729@tc.columbia.edu>
2018-08-18 19:51:45 -0400

    add skill component style

--- index.js
+++ index.js
@@ -37,9 +37,11 @@ var nav = {
 
 var sectionTitle = {
     template: `
-    <div class='section-title'>
-        <p class="order">{{order}}</p>
-        <p class="section-name">{{name}}</p>
+    <div class='section-title-wrap'>
+        <div class='section-title'>
+            <p class="order">{{order}}.</p>
+            <p class="section-name">{{name}}</p>
+        </div>
     </div>
     `,
     data: function(){
@@ -52,17 +54,47 @@ var sectionTitle = {
 
 var skillSection = {
     template: `
-        <section-title :order=order :name=sectionName></section-title>
-        <div class='section-content'>
-            <div class="content-description">{{description}}</div>
-            <div class="skillBar"></div>
+        <div class="skill-section each-section" :style="{background: sectionColor}">
+            <section-title :order=order :name=sectionName></section-title>
+            <div class='section-content'>
+                <div class="content-description">{{description}}</div>
+                <div class="skill-wrap">
+                    <div class="each-skill" v-for='i in skills'>
+                        <p class='skill-name'>{{i.name}}</p>
+                        <div class="bar-outter">
+                            <div class="bar-inner" :style="{width: i.extent + '%'}"></div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     `,
     data: function(){
         return {
             order: '01',
             sectionName: 'Skills',
-            description: 'lorem lopsium esf adeksm sdmamf emma da'
+            description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+            sectionColor: '#C93639',
+            skills: [
+                {
+                    name: 'HTML & CSS & JavaSCript',
+                    extent: '70'
+                },
+                {
+                    name: 'UX / UI Design',
+                    extent: '60'
+                },
+                {
+                    name: 'Videography',
+                    extent: '60'
+                },
+                {
+                    name: 'Vue / Riot / Firebase / MVC ...',
+                    extent: '70'
+                },
+            ]
+
         }
     },
 
