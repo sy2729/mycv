7984870443ba5c0f2d7ee14fda8b23324ae7fb27
sy2729 <sy2729@tc.columbia.edu>
2019-03-06 12:20:23 -0500

    add tooltips for switch langugae prompts and downaloading cv prompts

---
+++ js/chunk-2d0e523a.0956c2f2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e523a"],{"92e0":function(e,n,a){"use strict";a.r(n),a.d(n,"cvData",function(){return c});a("cadf"),a("551c"),a("f751"),a("097d");var c={header:{greeting:"您好，我是",target:"超有爱",name:"袁帅"}}}}]);
+//# sourceMappingURL=chunk-2d0e523a.0956c2f2.js.map
