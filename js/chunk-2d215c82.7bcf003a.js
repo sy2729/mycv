504fa7a379c96e8b8f899cd6269166686e20aa8e
sy2729 <sy2729@tc.columbia.edu>
2019-03-11 13:03:20 -0400

    fix a data import error

---
+++ js/chunk-2d215c82.7bcf003a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d215c82"],{c054:function(c,n,a){"use strict";a.r(n),a.d(n,"cvData",function(){return e});a("cadf"),a("551c"),a("f751"),a("097d");var e={header:{greeting:"你好，我是",name:"袁帅"},cvPath:{zh:"./cv/项目经理_袁帅_哥伦比亚大学_硕士_2019应届生.pdf"}}}}]);
+//# sourceMappingURL=chunk-2d215c82.7bcf003a.js.map
